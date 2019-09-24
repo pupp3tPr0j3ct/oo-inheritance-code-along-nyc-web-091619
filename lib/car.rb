d7c7653156a05ea947764143155e85174c2a6d25
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-24 14:04:35 -0400

    class Car passes tests

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,25 @@
 require_relative "./vehicle.rb"
+
+class Car
+
+    attr_accessor :wheel_size, :number, :wheel_number, :go, :fill_up_tank
+
+    def initialize(wheel_size, number)
+        @wheel_size = wheel_size
+        @number = number
+        @wheel_number = wheel_number
+        @go = go
+    end
+
+    def wheel_number
+        @wheel_number
+    end
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
+end
