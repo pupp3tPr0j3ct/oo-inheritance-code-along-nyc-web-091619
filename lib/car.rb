09145a049cffd6790f47289919b811c5b7a795c3
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-24 14:10:43 -0400

    Vehicle passes, cleaned car

--- lib/car.rb
+++ lib/car.rb
@@ -2,13 +2,11 @@ require_relative "./vehicle.rb"
 
 class Car
 
-    attr_accessor :wheel_size, :number, :wheel_number, :go, :fill_up_tank
+    attr_accessor :wheel_size, :number, :wheel_number
 
     def initialize(wheel_size, number)
         @wheel_size = wheel_size
         @number = number
-        @wheel_number = wheel_number
-        @go = go
     end
 
     def wheel_number
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,21 @@
 class Vehicle
 
+    attr_accessor :wheel_size, :number, :wheel_number
+
+    def initialize(wheel_size, number)
+        @wheel_size = wheel_size
+        @number = number
+    end
+
+    def wheel_number
+        @wheel_number
+    end
+
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
 end
